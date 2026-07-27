db78768822cf02f6010a25bf1796d1d35281cce5
msaf7524-prog <msaf7524@gmail.com>
2026-07-27 23:44:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.clock import Clock
-
 from jnius import autoclass
 
-TextToSpeech = autoclass('android.speech.tts.TextToSpeech')
-Locale = autoclass('java.util.Locale')
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
+PythonActivity = autoclass("org.kivy.android.PythonActivity")
+TextToSpeech = autoclass("android.speech.tts.TextToSpeech")
+Locale = autoclass("java.util.Locale")
 
 
 class VoiceApp(App):
     def build(self):
-        self.tts = None
-        btn = Button(text="Start Assistant")
-        btn.bind(on_press=self.speak)
-        Clock.schedule_once(self.init_tts, 0)
-        return btn
-
-    def init_tts(self, dt):
-        activity = PythonActivity.mActivity
-        self.tts = TextToSpeech(activity, None)
+        self.activity = PythonActivity.mActivity
+        self.tts = TextToSpeech(self.activity, None)
         self.tts.setLanguage(Locale.US)
 
+        btn = Button(text="Start Assistant")
+        btn.bind(on_press=self.speak)
+        return btn
+
     def speak(self, instance):
-        if self.tts:
+        if self.tts is not None:
             self.tts.speak(
                 "Hello, I am Voice Assistant 811",
-                TextToSpeech.QUEUE_FLUSH,
-                None,
-                "test"
+                0,
+                None
             )
 
 
