f282e3a0daaf991992b7c464159e3bc19017fc71
msaf7524-prog <msaf7524@gmail.com>
2026-07-28 06:08:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from jnius import autoclass
+from jnius import autoclass, PythonJavaClass, java_method
 
 PythonActivity = autoclass("org.kivy.android.PythonActivity")
 TextToSpeech = autoclass("android.speech.tts.TextToSpeech")
 Locale = autoclass("java.util.Locale")
 
 
+class TTSListener(PythonJavaClass):
+    __javainterfaces__ = ['android/speech/tts/TextToSpeech$OnInitListener']
+
+    @java_method('(I)V')
+    def onInit(self, status):
+        pass
+
+
 class VoiceApp(App):
     def build(self):
-        self.activity = PythonActivity.mActivity
-        self.tts = TextToSpeech(self.activity, None)
-        self.tts.setLanguage(Locale.US)
+        self.listener = TTSListener()
+        self.tts = TextToSpeech(PythonActivity.mActivity, self.listener)
 
         btn = Button(text="Start Assistant")
         btn.bind(on_press=self.speak)
         return btn
 
     def speak(self, instance):
-        if self.tts is not None:
-            self.tts.speak(
-                "Hello, I am Voice Assistant 811",
-                0,
-                None
-            )
+        self.tts.setLanguage(Locale.US)
+        self.tts.speak("Hello", 0, None)
 
 
 VoiceApp().run()
